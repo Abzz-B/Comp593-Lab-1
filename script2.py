63814c1c499f0e83f6656496ad878f0f865ee948
Abin <abin.binu@flemingcollege.ca>
2024-01-16 09:08:51 -0500

    Modified both script files
    Deleted print from script1.py
    Added comment to: to script2.py

--- script2.py
+++ script2.py
@@ -1,6 +1,7 @@
+# Print lines from Zen f Python
 print("Beautiful is better than ugly.")
 print("Explicit is better than implicit.")
-print("Simple is better than complex.
+print("Simple is better than complex.")
 print("Complex is better than complicated.")
 print("Flat is better than nested.")
 print("Sparse is better than dense.")
